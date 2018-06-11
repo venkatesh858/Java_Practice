f743f6adf95a435a6f4f9343a4035b05bf97552b
Venkatesham Boddu <venkatesh858@gmail.com>
2018-06-11 16:01:48 +0530

    master edit in Sample

--- Sample.java
+++ Sample.java
@@ -4,6 +4,8 @@ public class Sample{
 	System.out.println("Edited");
 	System.out.println("Hello");
 	System.out.println("Added a new message");
+	System.out.println("Added a new message");
+	System.out.println("Edit in master repo");
 	
 	}
 }
