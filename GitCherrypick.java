bd09605c82606e0166df188ee4da50822196937b
Venkatesham Boddu <venkatesh858@gmail.com>
2018-06-19 21:59:21 +0530

    master commit

---
+++ GitCherrypick.java
@@ -0,0 +1,6 @@
+public class GitCherrypick{
+	public static void main(String[] args){
+	System.out.println("First commit in dev1 for cherry pick");
+	System.out.println("First commit in dev1 for cherry pick");
+	}
+}
