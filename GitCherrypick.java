1dc23f0bfe81bb2242a25c90edd5781e13138b68
Venkatesham Boddu <venkatesh858@gmail.com>
2018-06-19 22:20:15 +0530

    hi

--- GitCherrypick.java
+++
@@ -1,6 +0,0 @@
-public class GitCherrypick{
-	public static void main(String[] args){
-	System.out.println("First commit in dev1 for cherry pick");
-	System.out.println("First commit in dev1 for cherry pick");
-	}
-}
