fb8eb0fd5966eb94cd59a9ab865b0d40d0d8e09a
Venkatesham Boddu <venkatesh858@gmail.com>
2018-06-19 21:47:12 +0530

    First commit in master for rebase

--- GitRebase.java
+++ GitRebase.java
@@ -2,5 +2,6 @@ public class GitRebase{
 	public static void main(String[] args){
 	System.out.println("First Commit in dev");
 	System.out.println("Second Commit in dev");
+	System.out.println("Commit in master for rebase");
 	}
 }
