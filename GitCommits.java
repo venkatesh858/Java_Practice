73cbd34bac9cf4e79c10534cfbb3e12825a34b4b
Venkatesham Boddu <venkatesh858@gmail.com>
2018-06-19 20:35:00 +0530

    Third Commit

--- GitCommits.java
+++ GitCommits.java
@@ -1,6 +1,7 @@
 public class GitCommits{
 	public static void main(String[] args){
-		System.out.println("First commit");
-		System.out.println("Second commit");
+		System.out.println("First commit Statement");
+		System.out.println("Second commit Statement");
+		System.out.println("Third commit Statement");
 	}
 }
